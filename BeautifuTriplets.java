95f8dc20f98ba37889674a147027906e2ea511b0
Alagar03 <145758867+AlagarR30@users.noreply.github.com>
2023-10-06 09:49:26 +0530

    Create BeautifuTriplets.java

---
+++ BeautifuTriplets.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'beautifulTriplets' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER d
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int beautifulTriplets(int d, List<Integer> arr) {
+    // Write your code here
+    int count=0;
+    for(int i=0;i<arr.size()-2;i++){
+        int tp1=arr.get(i);
+        for(int j=i+1;j<arr.size()-1;j++){
+            int tp2=arr.get(j);
+            for(int k=j+1;k<arr.size();k++){
+                int tp3=arr.get(k);
+                if(tp2-tp1!=d){
+                    break;
+                }
+                if(tp3-tp2==d){
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.beautifulTriplets(d, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
